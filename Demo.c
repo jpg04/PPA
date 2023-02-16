5b1b9f4a521dae56c693e20d6e155141d39bbec5
jpg04 <jaydeepgodase42161@gmail.com>
2023-02-16 16:12:34 +0530

    Demo Void Function

---
+++ Demo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void Display()
+{
+    printf("Shree Ganeshya");
+}
+
+int main()
+{
+    printf("inside The Main Function\n");
+    
+    Display(); //Function Call
+    return 0;
+}
+
+
